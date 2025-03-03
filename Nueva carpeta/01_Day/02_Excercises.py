e41b984cfc24ba1feed39a41aa444ebba53ef32d
UP240645 <up240645@alumnos.upa.edu.mx>
2025-03-03 00:27:26 -0600

    Delete Nueva carpeta directory

--- Nueva carpeta/01_Day/02_Excercises.py
+++
@@ -1,8 +0,0 @@
-#Ejercicio 2
-print(type(9.8))
-print(type(3.14))
-print(type(4-4j))
-print(type(["Asabeneh","Python","Findland"]))
-print(type("Angel Abner Soria Tiscareño"))
-print(type("Soria Tiscareño"))
-print(type("Mexico"))
