7a214756ee48e8dc63e64e83b5beb2dade4bc0d0
UP240645 <up240645@alumnos.upa.edu.mx>
2025-03-31 23:41:37 -0600

    Add files via upload

---
+++ python/10_Day/02_Exercise.py
@@ -0,0 +1,17 @@
+# Level 2
+# 1
+sum_nums = 0
+for i in range(0, 101):
+    sum_nums += i
+print("The sum of all numbers is", sum_nums)
+
+# 2
+even_sum = 0
+odd_sum = 0
+for i in range(0, 101):
+    if i % 2 == 0:
+        even_sum += i
+    else:
+        odd_sum += i
+print("The sum of all even numbers is", even_sum)
+print("The sum of all odd numbers is", odd_sum)
